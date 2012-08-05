0d5b0fc442eaceec945d03f09e2e7dc0d0e3f2b0
Pratik Khadloya <tispratik@gmail.com>
2012-08-05 16:13:58 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsforcharity::Application.config.secret_token = 'f8207822f8de3a7c189fa11ce0d01dca32a02e70b5975800c3299c2c23d36b8677fce8412c2cff70d652896134ea5e37939e4022d6638754f6fb24a4ee77601a'
