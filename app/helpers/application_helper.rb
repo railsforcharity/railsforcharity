8d8ac5f0a7d586c9db7014d05f8cb9e199154879
Pratik Khadloya <tispratik@gmail.com>
2012-08-25 17:12:27 -0700

    Faq updates

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,9 @@ module ApplicationHelper
   def user_not_signed_in?
     !user_signed_in?
   end
+
+  # Opens the link in a new tab/page
+  def link_to_blank(body, url, html_options = {})
+    link_to body, url, html_options.merge(:target => '_blank')
+  end
 end
