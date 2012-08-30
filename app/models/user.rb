ea86fa151f0f99c75b7630c11ed0a0bef084001a
Vibhuti <vinti.uiet@gmail.com>
2012-08-30 12:25:16 -0700

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,7 @@ class User < ActiveRecord::Base
     where(auth.slice(:provider, :uid)).first_or_create do |user|
       user.provider = auth.provider
       user.uid = auth.uid
+      user.email = auth.email
     end
   end
 
