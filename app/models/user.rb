b56e898d0e0ce00d62f0a2027c09d48785f94ec6
Vibhuti <vinti.uiet@gmail.com>
2012-08-30 13:15:24 -0700

    mend
    fix omniauth email updation problem

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,8 @@ class User < ActiveRecord::Base
     where(auth.slice(:provider, :uid)).first_or_create do |user|
       user.provider = auth.provider
       user.uid = auth.uid
-      user.email = auth.email
+      user.email = auth.info.email
+      p '---------'
     end
   end
 
