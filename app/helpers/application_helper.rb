f3ab2fb976a99721f174f7f3815425c27e66a791
Pratik Khadloya <tispratik@gmail.com>
2012-09-03 19:42:36 -0700

    Sponsor updates

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
   end
 
   # Opens the link in a new tab/page
-  def link_to_blank(body, url, html_options = {})
+  def link_to_blank(body, url=body, html_options = {})
     link_to body, url, html_options.merge(:target => '_blank')
   end
 end
