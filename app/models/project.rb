a3a82b89f1ca1af862a78c04a2ffe5e80ea29d21
Vibhuti <vinti.uiet@gmail.com>
2012-09-04 17:52:33 -0700

    pluralizing model name for has_and_belongs_to_many associations

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,17 @@
+# == Schema Information
+#
+# Table name: projects
+#
+#  id          :integer          not null, primary key
+#  name        :string(255)
+#  title       :string(255)
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Project < ActiveRecord::Base
-  attr_accessible :description, :name, :title
+  attr_accessible :description, :name, :title, :user_ids
 
   # Relations
   has_and_belongs_to_many :users
@@ -9,4 +21,5 @@ class Project < ActiveRecord::Base
   validates :title, :presence => true, :length => { :in => 10..140 }
   validates :description, :presence => true, :length => { :in => 50..2000 }
 
+  accepts_nested_attributes_for :users
 end
