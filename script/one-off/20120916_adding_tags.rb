a63152195e2d3b6577584df52ac7580537f092da
Vibhuti <vinti.uiet@gmail.com>
2012-09-16 10:47:27 -0700

    adding one-off script for tasks

--- script/one-off/20120916_adding_tags.rb
+++ script/one-off/20120916_adding_tags.rb
@@ -1,8 +1,15 @@
 require File.dirname(File.expand_path(__FILE__)) + "/../../config/environment"
 
-tags = ['Education', 'Raising Funds', 'Feeding Homeless People', 'Health Care', 'Agricultural Production', 'Sustainable Construction', 'Citizen Activism', 'Empowerment', 'Childhood Education', 'Affordable Transportation', 'Community Solidarity', 'Efficacy']
+project_tags = ['Education', 'Raising Funds', 'Feeding Homeless People', 'Health Care', 'Agricultural Production', 'Sustainable Construction', 'Citizen Activism', 'Empowerment', 'Childhood Education', 'Affordable Transportation', 'Community Solidarity', 'Efficacy']
+task_tags = ['Ruby', 'Java', 'Javascript', 'Jquery', 'CSS', 'Node.js', 'SASS', 'Coffeescript', 'Ruby on Rails', 'Deployment', 'Management', 'Marketing', 'Legal', 'Design', 'Sales', 'Art', 'Analytics', 'Operations', 'Maintenance']
+other_tags = ['Management', 'Marketing', 'Legal', 'Design', 'Sales', 'Art', 'Analytics', 'Operations', 'Maintenance']
 
-tags.each do |tag_name|
+project_tags.each do |tag_name|
   p "Creating #{tag_name}"
   Tag.create(:name => tag_name, :tag_type => 'project')
+end
+
+task_tags.each do |tag_name|
+  p "creating #{tag_name}"
+  Tag.create(name: tag_name, tag_type: 'task')
 end
