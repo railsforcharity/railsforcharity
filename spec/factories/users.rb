1511c0eda094ea0ca60e1b26078d6b293682c613
Vibhuti <vinti.uiet@gmail.com>
2012-09-16 18:04:50 -0700

    project index page updates

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :user do
+  factory :user, aliases: [:creator] do
     name { Faker.name }
     email { Faker::Internet.email }
     password 'secret'
