99522103280d5f81a8936ced16f70d71c811f7a6
Vibhuti <vinti.uiet@gmail.com>
2012-09-24 00:20:30 -0700

    fixing project index collaborators images

--- app/presenters/user_presenter.rb
+++ app/presenters/user_presenter.rb
@@ -1,9 +1,9 @@
 class UserPresenter < ApplicationPresenter
   presents :user
 
-  def avatar
+  def avatar(size)
     img = 'default.png'
     img = user.avatar.image_url(:thumb).to_s if user.avatar
-    h.image_tag img, :height => '80', :width => '80'
+    h.image_tag img, :height => size, :width => size
   end
 end
