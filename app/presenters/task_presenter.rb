b8e79b9a066bb406867785d44016ad97ec1df850
Pratik Khadloya <tispratik@gmail.com>
2012-09-29 15:31:45 -0700

    Task editing functionality

--- app/presenters/task_presenter.rb
+++ app/presenters/task_presenter.rb
@@ -5,4 +5,12 @@ class TaskPresenter < ApplicationPresenter
     "#{task.estimated_hours} hr #{task.estimated_minutes} minutes"
   end
 
+  def creator_name
+    task.creator.name
+  end
+
+  def assignee_name
+    task.assignee.name
+  end
+
 end
