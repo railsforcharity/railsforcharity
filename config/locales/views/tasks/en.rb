5793054d35555a62dc5d7c87afade7363ebeaa11
Pratik Khadloya <tispratik@gmail.com>
2012-10-07 20:44:46 -0700

    Task help page

---
+++ config/locales/views/tasks/en.rb
@@ -0,0 +1,17 @@
+{
+  en: {
+    views: {
+      tasks: {
+        help: {
+          project: "Select the project this task is to be created under.",
+          title: "Give a small and descriptive title for your task in less than 140 characters. ",
+          description: "Describe in detail what this task is about. The detailed description helps people new to the project to understand exactly what needs to be done.",
+          tags: "Select appropriate tags for the task so that similar tasks are grouped together and people searching for tasks may be able to find it.",
+          estimated_time: "Give an estimate of the time that will be required to complete this task.",
+          category: 'Categorize your task so that the right people pick up your task for working.',
+          type: 'If its a programming task then select the type.'
+        }
+      }
+    }
+  }
+}
