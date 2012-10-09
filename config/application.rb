e4ce3d088433483f90c2a400eaa2ad255bd7c1c4
Pratik Khadloya <tispratik@gmail.com>
2012-10-09 06:14:55 -0700

    some changes in the Rails 3.2 initialization process that conflicts with Heroku slug compilation
    This option is new in Rails 3.2 and prevents the Rails environment to be loaded when the assets:precompile task is executed. Because Heroku precompile assets before setting the database configuration, you need to set this configuration to false or you Rails application will try to connect to an unexisting database.

--- config/application.rb
+++ config/application.rb
@@ -58,6 +58,8 @@ module Railsforcharity
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    config.assets.initialize_on_precompile = false
+
     config.generators do |g|
       g.view_specs false
       g.helper_specs false
