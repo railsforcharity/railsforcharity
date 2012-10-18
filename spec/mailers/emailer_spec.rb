1916b397c732938d235fd03a70d849061235738c
Pratik Khadloya <tispratik@gmail.com>
2012-10-18 07:16:38 -0700

    More email specs

--- spec/mailers/emailer_spec.rb
+++ spec/mailers/emailer_spec.rb
@@ -1,5 +1,22 @@
 require "spec_helper"
 
 describe Emailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { build(:user) }
+  let(:project) { build(:project) }
+
+  describe 'new_task email' do
+    let(:mail) { Emailer.send_email(user, :new_task, project) }
+
+    it 'renders the sender email' do
+      mail.from.should == ['donotreply@railsforcharity.org']
+    end
+
+    it 'renders the receiver email' do
+      mail.to.should == [user.email]
+    end
+
+    it 'renders the project name in the subject' do
+      mail.subject.should match project.name
+    end
+  end
 end
