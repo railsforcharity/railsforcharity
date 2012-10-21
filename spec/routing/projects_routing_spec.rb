b81d7a31f56a8fe8915e1e93d3e030ddc704cd51
Pratik Khadloya <tispratik@gmail.com>
2012-10-21 10:43:24 -0700

    Fix broken specs

--- spec/routing/projects_routing_spec.rb
+++ spec/routing/projects_routing_spec.rb
@@ -4,31 +4,31 @@ describe ProjectsController do
   describe "routing" do
 
     it "routes to #index" do
-      get("/projects").should route_to("projects#index")
+      get("/p").should route_to("projects#index")
     end
 
     it "routes to #new" do
-      get("/projects/new").should route_to("projects#new")
+      get("/p/new").should route_to("projects#new")
     end
 
     it "routes to #show" do
-      get("/projects/1").should route_to("projects#show", :id => "1")
+      get("/p/1").should route_to("projects#show", :id => "1")
     end
 
     it "routes to #edit" do
-      get("/projects/1/edit").should route_to("projects#edit", :id => "1")
+      get("/p/1/edit").should route_to("projects#edit", :id => "1")
     end
 
     it "routes to #create" do
-      post("/projects").should route_to("projects#create")
+      post("/p").should route_to("projects#create")
     end
 
     it "routes to #update" do
-      put("/projects/1").should route_to("projects#update", :id => "1")
+      put("/p/1").should route_to("projects#update", :id => "1")
     end
 
     it "routes to #destroy" do
-      delete("/projects/1").should route_to("projects#destroy", :id => "1")
+      delete("/p/1").should route_to("projects#destroy", :id => "1")
     end
 
   end
