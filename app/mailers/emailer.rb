3ce125edca5240a454111d3be503ec4c2e51c2d5
Vibhuti <vinti.uiet@gmail.com>
2012-10-31 06:32:52 -0700

    improve new task creation email

--- app/mailers/emailer.rb
+++ app/mailers/emailer.rb
@@ -19,25 +19,22 @@ class Emailer < ActionMailer::Base
     subject_template = ERB.new(EmailTemplate::TYPES[email_type][:subject])
     subject = subject_template.result(binding)
     @user = recipients.first
-
-    mail(
-      :to => recipients.map(&:email).join(","),
-      :subject => "[RailsforCharity] #{subject}"
-    ) do |format|
-      format.html { render "emailer/email_templates/#{email_type}" }
+    recipients.each do |recipient|
+      mail(
+        :to => recipient.email,
+        :subject => "[RailsforCharity] #{subject}"
+      ) do |format|
+        format.html { render "emailer/email_templates/#{email_type}" }
+      end
     end
   end
 
-  def send_task_email(recipients, email_type, project, task)
+  def send_task_email(recipient, email_type, project, task)
     subject_template = ERB.new(EmailTemplate::TYPES[email_type][:subject])
     subject = subject_template.result(binding)
     @task = task
-    @user = recipients.first
-
-    mail(
-      :to => recipients.map(&:email).join(","),
-      :subject => "[RailsforCharity] #{subject}"
-    ) do |format|
+    @user = recipient
+    mail(:to => @user.email, :subject => "[RailsforCharity] #{subject}") do |format|
       format.html { render "emailer/email_templates/#{email_type}" }
     end
   end
