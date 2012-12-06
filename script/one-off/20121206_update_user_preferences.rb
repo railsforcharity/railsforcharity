0b60fa2d37c27757450663ff934c39b528a65245
Pratik Khadloya <tispratik@gmail.com>
2012-12-06 05:53:27 -0800

    Script to update user preferences

---
+++ script/one-off/20121206_update_user_preferences.rb
@@ -0,0 +1,18 @@
+require File.dirname(File.expand_path(__FILE__)) + "/../../config/environment"
+
+project = Project.find(ENV['PROJECT_ID'])
+project.users.each do |user|
+  preference = Preference.user_project_preference(user, project)
+  next if preference
+
+  properties = {}
+  EmailTemplate::TYPES.map do |property_name, property_details|
+    properties[property_name.to_s] = "1"
+  end
+
+  Preference.create({
+    user: user,
+    entity: project,
+    properties: properties
+  })
+end
