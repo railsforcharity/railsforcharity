27818744604b89a867e1574c379b56f9eb28e919
Pratik Khadloya <tispratik@gmail.com>
2012-12-13 00:05:50 -0800

    Fix users index page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,7 +9,7 @@ module ApplicationHelper
     link_to name, link_url, link_url_html_options.merge(:target => '_blank')
   end
 
-  def link_to_image(image_url, image_html_options = {}, link_url=name, link_url_html_options = {})
+  def link_to_image(image_url, image_html_options = {}, link_url='#', link_url_html_options = {})
     link_to image_tag(image_url, image_html_options.merge(alt: image_html_options[:title])), link_url, link_url_html_options.merge(:target => '_blank')
   end
 
